a6504f2f2ba60b808db42740d109df9f0cbdbe9c
Matt Robinson <mrmrrobinson@gmail.com>
2021-02-08 19:53:00 +0000

    [Updated] - Bow procedure.

---
+++ src/main/java/mattonfire/dnd/classes/mixin/BowItemMixin.java
@@ -0,0 +1,28 @@
+package mattonfire.dnd.classes.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BowItem;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import mattonfire.dnd.classes.PlayerEntityExt;
+
+@Mixin(BowItem.class)
+public class BowItemMixin {
+    @ModifyArgs(method = "Lnet/minecraft/item/BowItem;onStoppedUsing()V", 
+        at = @At(
+        value = "INVOKE", 
+        target = "Lnet/minecraft/entity/projectile/PersistentProjectileEntity;setProperties(Lnet/minecraft/entity/Entity;FFFFF)V")
+    )
+    private void injected(Args args) {
+        PlayerEntity playerEntity = args.get(0);
+        float a4 = args.get(4);
+        if(((PlayerEntityExt)playerEntity).dndClassExist()==8){
+            args.set(4, a4*3);
+        }
+    }
+}
---
+++ src/main/java/mattonfire/dnd/classes/mixin/PreAbstractClientPlayerEntityMixin.java
@@ -0,0 +1,57 @@
+package mattonfire.dnd.classes.mixin;
+
+import java.util.Optional;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import mattonfire.dnd.classes.PlayerEntityExt;
+
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(value = AbstractClientPlayerEntity.class, priority = 10)
+public abstract class PreAbstractClientPlayerEntityMixin extends PlayerEntity {
+  private float ZOOM_MODIFIER = 3.f;
+  public PreAbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
+    super(world, pos, yaw, profile);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Inject(at = @At("TAIL"), method = "getSpeed()F", cancellable = true)
+  private float getSpeed(CallbackInfoReturnable<Float> cb) {
+    PlayerEntity playerEntity = (PlayerEntity)(Object)this;
+
+    float modifier = 1.0F;
+
+    if (playerEntity.isUsingItem() && playerEntity.getActiveItem().getItem() == Items.BOW) {
+      int i = playerEntity.getItemUseTime();
+      float g = (float) i / 20.0F;
+
+      if (g > 1.0F) {
+        g = 1.0F;
+      } else {
+        g *= g;
+      }
+      if(((PlayerEntityExt)playerEntity).dndClassExist()==8){
+        modifier *= 1.0F - g * 0.15F * ZOOM_MODIFIER;
+      } else {
+        modifier *= 1.0F - g * 0.15F;
+      }
+    }
+    return MathHelper.lerp(MinecraftClient.getInstance().options.fovEffectScale, 1.0F, modifier);
+  }
+}
